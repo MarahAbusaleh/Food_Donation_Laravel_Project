d02c18b9723da6a2e8dfea3f81baebef69f203b1
MarahAbusaleh <marah.abusaleh12@gmail.com>
2023-09-07 23:07:25 +0300

    Marah Added

---
+++ app/Http/Controllers/VolanteerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Volanteer;
+use Illuminate\Http\Request;
+
+class VolanteerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Volanteer  $volanteer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Volanteer $volanteer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Volanteer  $volanteer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Volanteer $volanteer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Volanteer  $volanteer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Volanteer $volanteer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Volanteer  $volanteer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Volanteer $volanteer)
+    {
+        //
+    }
+}
---
+++ app/Models/UserDonation.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserDonation extends Model
+{
+    use HasFactory;
+
+    //With Donation Model
+    public function donation() 
+    {
+        return $this->belongsTo(Donation::class);
+    }
+
+    //With User Model
+    public function user() 
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    //With PaymentDetails Model
+    public function paymenydetails() 
+    {
+        return $this->belongsTo(PaymentDetails::class);
+    }
+}
